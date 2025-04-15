c2a750a8b6034461b1576314f85a2bcf5697a3ab
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-04-16 00:08:56 +0530

    feat: update react-router-dom to version 7.5.0 and implement navigation links in App component

--- machinecodingreact/src/App.jsx
+++ machinecodingreact/src/App.jsx
@@ -1,12 +1,35 @@
+import { Suspense } from 'react'
 import './App.css'
 import { SearchBar } from './Questions'
+import { Routes ,Route, Link, NavLink} from 'react-router-dom'
 
 function App() {
+  const links = [
+    { to: "/SearchBar", label: "Search Bar" },
+    { to: "/ThemeContext" , label:"Theme Context"}
+  ]
   return (
     <>
       <div className='h-full w-full font-bold'>
         <h1 className='text-center'> HELLO WORLD </h1>
-        <SearchBar/>
+        {
+          links.map((item , key)=>{
+            return (
+              <NavLink 
+                className={({isActive})=>(isActive?"text-amber-600":"text-blue-950")} 
+                key={key} 
+                to={item.to}
+              >
+                {item.label} |
+              </NavLink>
+            )
+          })
+        }
+        <Suspense fallback={<>Loading...</>}>
+          <Routes>
+            <Route path="/SearchBar" element={<SearchBar />} />
+          </Routes>
+        </Suspense>
       </div>
     </>
   )
